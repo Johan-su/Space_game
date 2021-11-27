2cdbfd6fa3c12e13ad125ad90fb49c2b50b10f76
Johan <johan@desktop-riovii.nidnus.nu>
2021-11-27 21:47:46 +0100

    Added function decs

--- src/ecs/EntityManager.h
+++ src/ecs/EntityManager.h
@@ -1,10 +1,29 @@
 #pragma once
+#include <cstdint>
+
+
+struct Entity //TODO(johan): move to a better place.
+{
+  uint64_t id;
+};
 
 class EntityManager
 {
- public:
+public:
+  void init();
+  void clean();
 
- private:
+  Entity create_entity();
+  void set_entity_signature(/*signature goes here*/);
+  void get_entity_signature(Entity e);
+
+
+
+private:
+
+  //signature[] m_signature;
+
+
 
 
 };
