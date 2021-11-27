8855379a5604a6c46a1b7f132a8d7cab784895d5
Johan <johan@desktop-riovii.nidnus.nu>
2021-11-27 21:42:58 +0100

    implemented macro, converting fields to array.

--- src/ecs/Components.h
+++ src/ecs/Components.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <cstdint>
+
+#define to_array_field(type, name) type name[MAX_ENTITY_AMOUNT];
+
+#define PositionComponent \
+  to_array_field(float, x) \
+  to_array_field(float, y) \
+
+#define HealthComponent \
+  to_array_field(float, health) \
+  to_array_field(float, health_regen) \
+
+struct Component_data
+{
+  PositionComponent
+  HealthComponent
+};
+
+#undef to_array_field
