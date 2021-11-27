260d3b19a17f0d715f7f515b148206f62947f04b
Johan <johan@desktop-riovii.nidnus.nu>
2021-11-27 21:45:22 +0100

    Added central ecs header.

--- src/ecs/ecs.h
+++ src/ecs/ecs.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "ecs_constants.h"
+
+
+
+#include "MemoryManager.h"
+
+#include "Components.h"
+#include "ComponentManager.h"
+#include "SystemManager.h"
+#include "EntityManager.h"
