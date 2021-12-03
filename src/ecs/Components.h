db50b32b1c7f9de6619e4665b159e2c2998787c8
Johan <johan@desktop-riovii.nidnus.nu>
2021-12-03 23:32:22 +0100

    Macro to create component and SOA.

--- src/ecs/Components.h
+++ src/ecs/Components.h
@@ -1,20 +1,58 @@
 #pragma once
+#include "Entity.h"
+#include "ecs_constants.h"
 #include <cstdint>
 
-#define to_array_field(type, name) type name[MAX_ENTITY_AMOUNT];
+//#define _DEBUG
 
-#define PositionComponent \
+
+#define ENTITY_LIST_INDICIES Entity EntityList[MAX_ENTITY_AMOUNT]; \
+   uint64_t EntityIndices[MAX_ENTITY_AMOUNT]; \
+
+
+#define POSITIONLIST \
   to_array_field(float, x) \
   to_array_field(float, y) \
 
-#define HealthComponent \
+#define HEALTHLIST \
   to_array_field(float, health) \
   to_array_field(float, health_regen) \
 
-struct Component_data
-{
-  PositionComponent
-  HealthComponent
-};
+
+#ifndef _DEBUG
+
+#define STRUCT_GEN(NAME, LIST)                  \
+  struct NAME ## Component                      \
+  {LIST};                                       \
+
+#else
+
+#define STRUCT_GEN(NAME, LIST)                  \
+  struct NAME ## Component                      \
+  {LIST Entity currentEntity;};                 \
+
+#endif
+
+
+
+#define ARRAY_GEN(NAME, LIST)                   \
+  struct NAME ## Array                          \
+  {LIST ENTITY_LIST_INDICIES};                  \
+
+
+#define to_array_field(TYPE, NAME) TYPE NAME;
+
+STRUCT_GEN(Position, POSITIONLIST)
+STRUCT_GEN(Health, HEALTHLIST)
+
+#undef to_array_field
+
+#define to_array_field(TYPE, NAME) TYPE NAME[MAX_ENTITY_AMOUNT]; \
+  uint64_t NAME ## _size;                                        \
+
+
+ARRAY_GEN(Position, POSITIONLIST)
+ARRAY_GEN(Health, HEALTHLIST)
+
 
 #undef to_array_field
