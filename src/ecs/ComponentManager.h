d4526dfdfa0c7eb0cbcabec8c031db818ae3ae46
Johan <johan@desktop-riovii.nidnus.nu>
2021-12-03 23:38:17 +0100

    begun implementing get_component from array.

--- src/ecs/ComponentManager.h
+++ src/ecs/ComponentManager.h
@@ -1,17 +1,33 @@
 #pragma once
+#include "ecs_constants.h"
 #include "Components.h"
 #include <cstdint>
+#include <cstddef>
+
 
 
 class ComponentManager
 {
- public:
+    public:
 
 
+        template <typename T>
+        void set_component(Entity e)
+        {
 
+        }
 
- private:
+        template <typename T>
+        T get_component(Entity e)
+        {
+            auto pc = PositionComponent();
+            pc.x = m_positionArray.x[e.id];
+            pc.y = m_positionArray.y[e.id];
 
+            return static_cast<T>(pc);
+        }
+
+    private:
+        PositionArray m_positionArray;
+        HealthArray m_healthArray;
 };
-
-
