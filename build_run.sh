4a9f97b9a26d24abbc034dffaec0e4471522397c
Johan <johan@desktop-riovii.nidnus.nu>
2021-12-03 23:37:15 +0100

    inlined commands to make copying easier.

--- build_run.sh
+++ build_run.sh
@@ -1,4 +1,1 @@
-mkdir build
-cmake -S . -B ./build
-cmake --build ./build
-./build/Space_game
+cd ~/repos/Space_game && mkdir -p build  && cmake -S . -B ./build && cmake --build ./build && ./build/Space_game
