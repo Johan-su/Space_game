2f67ae7c6c2d2e9e0d006571606977918900adf3
Johan <>
2021-12-19 22:11:18 +0100

    begun adding boilerplate

---
+++ src/ecs/SystemManager.cpp
@@ -0,0 +1,15 @@
+#include "SystemManager.hpp"
+
+bool SystemManager::init()
+{
+    return 0;
+}
+
+bool SystemManager::clean()
+{
+    return 0;
+}
+void SystemManager::set_entity_signature(Entity e, Signature sig)
+{
+    
+}
--- src/ecs/SystemManager.hpp
+++ src/ecs/SystemManager.hpp
@@ -1,1 +1,15 @@
 #pragma once
+#include "Entity.hpp"
+#include "Signature.hpp"
+class SystemManager
+{
+public:
+
+bool init();
+bool clean();
+
+void set_entity_signature(Entity e, Signature sig);
+
+private:
+
+};
