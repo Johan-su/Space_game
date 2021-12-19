600dce5b5fe5b01a01cae407381fa07263386d67
Johan <>
2021-12-19 22:08:43 +0100

    added debug macro for assert, made assert exit

--- src/ecs/ecs_assert.hpp
+++ src/ecs/ecs_assert.hpp
@@ -1,12 +1,18 @@
 #pragma once
 #include <iostream>
+#ifndef DDEBUG
 #define assert(condition, message) \
  do \
 { \
    if(!(condition)) \
    { \
-      std::cerr << "Assertion " << " #condition " \
+      std::cerr << "Assertion " << "[ " #condition " ]" \
       << " failed in " << __FILE__ << " line " << __LINE__ \
-      << ": " << message << "\n"; \
+      << ": " << message << "\n"; exit(1); \
    } \
  } while(0)
+
+#else
+ do {} while(0) \
+
+ #endif
