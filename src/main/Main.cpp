b910076b33eb41a1599a58c26752f88dd9424f51
Johan <>
2021-12-19 22:12:10 +0100

    isSame function, component_test removed old stuff

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -1,5 +1,6 @@
 #include "../ecs/ecs.hpp"
 #include "../ecs/globals.hpp"
+#include "../ecs/Registry.hpp"
 #include "../ecs/ecs_assert.hpp"
 
 #include <cstdint>
@@ -7,9 +8,32 @@
 #include <SDL.h>
 
 
+typedef byte uint8_t;
+
+template<typename T>
+bool isSame(T first, T second)
+{
+    size_t typesize =  sizeof(T);
+
+    auto *fp = (byte*)&first;
+    auto *sp = (byte*)&second;
+
+    bool result = true;
+
+    for(size_t i = 0; i < typesize; ++i)
+    {
+        if(*fp != *sp)
+        {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
 
 
-Globaldata g_data;
+Globaldata glo;
 
 void mem_test()
 {
@@ -37,43 +61,48 @@ void mem_test()
 
 void component_test()
 {
-    
+    Entity e =  glo.reg->create_entity();
+    auto pc = PositionComponent();
+    pc = {420.0f, 69.0f};
+    glo.reg->set_component(e, pc);
+
+    auto pc1 = glo.reg->get_component<PositionComponent>(e);
+
+    bool test = isSame<PositionComponent>(pc, pc1);
+    std::cout << "pc == pc1: " << test << "\n";
+
 }
 
 
 void ecs_test()
 {
-
 }
 
 bool ecs_init()
 {
-    //std::cout << "size of mem " << sizeof(MemoryManager) << "\n";
-    g_data.mem = new MemoryManager();
-    g_data.mem->init();
-    g_data.compm = g_data.mem->alloc<ComponentManager>();
-    g_data.compm->init(g_data.mem);
-    g_data.enm = g_data.mem->alloc<EntityManager>();
-    g_data.enm->init(g_data.mem);
+    glo.reg = new Registry();
+
+    if(glo.reg->init())
+    {
+        return 1;
+    }
     return 0;
 }
 bool ecs_clean()
 {
-    g_data.compm->clean(g_data.mem);
-    g_data.mem->clean();
+    glo.reg->clean();
 
-    delete g_data.mem;
+    delete glo.reg;
+    glo.reg = nullptr;
 
-    g_data.compm = nullptr;
-    g_data.mem = nullptr;
     return 0;
 }
 bool init()
 {
     if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS))
     {
-      std::cout << "SDL_INITALIZATION FAILED " << std::endl;
-      std::cout << SDL_GetError() << std::endl;
+      std::cout << "SDL_INITALIZATION FAILED " << "\n";
+      std::cerr << SDL_GetError() << std::endl;
       return 1;
     }
 
@@ -114,7 +143,6 @@ int main(int argc, char *argv[])
     exit(clean_exit_code);
     return 0;
 }
-
 
 
 
