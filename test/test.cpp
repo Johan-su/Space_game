ead56d503b0f4973278b6b661d7d569e4a11dcba
Johan <>
2021-12-20 20:47:56 +0100

    Begun on testing framework

---
+++ test/test.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <syscall.h>
+#include <cstring>
+#include <string>
+
+bool testFile(const char *filepath)
+{
+    char cmds[200];
+    strcat(cmds, "echo ");
+    strcat(cmds, filepath);
+    strcat(cmds, "| > output_");
+    strcat(cmds, filepath);
+    strcat(cmds, "_.txt");
+    system(cmds);
+    return 0;
+}
+
+bool load_test_output(const char *filepath)
+{
+    FILE *fp = fopen(filepath, "r");
+    if(!fp)
+    {
+        return 1;
+    }
+    fscanf(fp, )
+
+    fclose(fp);
+    return 0;
+}
+
+bool testFiles(const char *dirpath)
+{
+    char cmds[1000];
+    strcat(cmds, "cd dirpath &&");
+    strcat(cmds, " ")
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+    auto test = testFile("random/path");
+
+
+    return 0;
+}
