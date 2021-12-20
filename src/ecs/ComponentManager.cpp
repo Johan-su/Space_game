114008247c1c63702083550784912815ecb7fbb1
Johan <>
2021-12-20 12:36:20 +0100

    fixed initalization for arrays

--- src/ecs/ComponentManager.cpp
+++ src/ecs/ComponentManager.cpp
@@ -2,15 +2,26 @@
 #include "Components.hpp"
 #include "ComponentManager.hpp"
 
+#include <cstring>
 
-bool ComponentManager::init(MemoryManager* mem)
+
+
+bool ComponentManager::init(MemoryManager *mem)
 {
     m_componentIdCount = 1;
-    
     m_positionArray = mem->alloc<PositionArray>();
+
+    memset(m_positionArray->EntityIndices, 0xFF, sizeof(uint64_t) * MAX_ENTITY_AMOUNT);
+    memset(m_positionArray->EntityList, 0, sizeof(Entity) * MAX_ENTITY_AMOUNT);
+
+    m_positionArray->EntityAmount = 0;
+
+
     m_healthArray = mem->alloc<HealthArray>();
+
     return 0;
 }
+
 bool ComponentManager::clean(MemoryManager* mem)
 {
     mem->dealloc(m_positionArray);
