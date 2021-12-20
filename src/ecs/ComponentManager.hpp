9a96f1b0e86a453be7ea7e3f8488ffd5036a6b1b
Johan <>
2021-12-20 12:36:41 +0100

    begun adding component_array functions

--- src/ecs/ComponentManager.hpp
+++ src/ecs/ComponentManager.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "Components.hpp"
 #include "ecs_constants.hpp"
+#include "Entity.hpp"
 #include "MemoryManager.hpp"
 #include "ecs_assert.hpp"
 #include <cstdint>
@@ -10,23 +11,67 @@
 class ComponentManager
 {
 public:
-    bool init(MemoryManager* mem);
-    bool clean(MemoryManager* mem);
+    bool init(MemoryManager *mem);
+    bool clean(MemoryManager *mem);
+
+private:
+    template<typename T>
+    bool array_init(T *array, MemoryManager *mem)
+    {
+        array = mem->alloc<T>();
+        return 0;
+    }
 
 public:
     template <typename T>
-    void set_component(Entity e, T comp)
+    void set_component(Entity e, T& comp)
     {
         assert(false, "non specialized template");
     }
     template <>
-    void set_component<PositionComponent>(Entity e, PositionComponent comp)
+    void set_component<PositionComponent>(Entity e, PositionComponent& comp)
     {
-        auto& elist =  m_positionArray->EntityList;
-        auto& eind =  m_positionArray->EntityIndices;
+        Entity *elist =  m_positionArray->EntityList;
+        uint64_t *eind =  m_positionArray->EntityIndices;
+        uint64_t &eAmount = m_positionArray->EntityAmount;
 
-        m_positionArray->x[elist[eind[e.id]].id] = comp.x;
-        m_positionArray->y[elist[eind[e.id]].id] = comp.y;        
+
+        eind[e.id] = eAmount++;
+
+        elist[eind[e.id]] = e;
+
+
+        m_positionArray->x[eind[e.id]] = comp.x;
+        m_positionArray->y[eind[e.id]] = comp.y;        
+    }
+
+    template<typename T>
+    void destroy_component(Entity e)
+    {
+        assert(false, "non specialized template");       
+    }
+    template<>
+    void destroy_component<PositionComponent>(Entity e)
+    { // https://gist.github.com/dakom/82551fff5d2b843cbe1601bbaff2acbf
+    
+        Entity *elist =  m_positionArray->EntityList;
+        uint64_t *eind =  m_positionArray->EntityIndices;
+        uint64_t &eAmount = m_positionArray->EntityAmount;
+
+
+
+        Entity lastentity = elist[--eAmount];
+        uint64_t lastentity_index = eind[lastentity.id];
+        eind[lastentity.id] = eind[e.id];
+        m_positionArray->x[eind[e.id]] = m_positionArray->x[lastentity_index];
+        m_positionArray->y[eind[e.id]] = m_positionArray->y[lastentity_index];
+        
+        elist[eAmount] = {0};
+        elist[eind[lastentity.id]] = lastentity;
+        elist[eind[e.id]] = {0};
+
+        eind[e.id] = 0;
+
     }
 
 
@@ -44,8 +89,8 @@ public:
         auto& eind =  m_positionArray->EntityIndices;
 
         auto pc = PositionComponent();
-        pc.x = m_positionArray->x[elist[eind[e.id]].id];
-        pc.y = m_positionArray->y[elist[eind[e.id]].id];
+        pc.x = m_positionArray->x[eind[e.id]];
+        pc.y = m_positionArray->y[eind[e.id]];
         return pc;
     }
 
