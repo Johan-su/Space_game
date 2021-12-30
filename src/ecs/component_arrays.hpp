3f90185a4966bfa41264e4dd9df803b94e91dbeb
Johan-su <>
2021-12-30 23:02:06 +0100

    fix, entity_list and entity_indicies are arrays

--- src/ecs/component_arrays.hpp
+++ src/ecs/component_arrays.hpp
@@ -6,7 +6,7 @@
 
 // struct component array generator
 #define STRUCT_GEN(NAME, vargs...) \
-struct NAME ## _array {Entity entity_list; Entity entity_indicies; size_t array_size; vargs};
+struct NAME ## _array {Entity entity_list[MAX_ENTITY_AMOUNT]; Entity entity_indicies[MAX_ENTITY_AMOUNT]; size_t array_size; vargs};
 
 #define DATA_GEN(TYPE, VAR) \
 TYPE VAR[MAX_ENTITY_AMOUNT];
@@ -15,47 +15,3 @@ COMPONENT_LIST(STRUCT_GEN, DATA_GEN)
 
 #undef STRUCT_GEN
 #undef DATA_GEN
-
-
-/*#define ENTITY_LIST_INDICIES Entity EntityList[MAX_ENTITY_AMOUNT]; \
-   uint64_t EntityIndices[MAX_ENTITY_AMOUNT]; \
-   uint64_t EntityAmount; \
-
-
-#define POSITIONLIST \
-  to_array_field(float, x) \
-  to_array_field(float, y) \
-
-#define HEALTHLIST \
-  to_array_field(float, health) \
-  to_array_field(float, health_regen) \
-
-
-#define STRUCT_GEN(NAME, LIST)                  \
-  struct NAME ## Component                      \
-  {LIST};                                       \
-
-
-
-#define ARRAY_GEN(NAME, LIST)                   \
-  struct NAME ## Array                          \
-  {LIST ENTITY_LIST_INDICIES};                  \
-
-
-#define to_array_field(TYPE, NAME) TYPE NAME;
-
-STRUCT_GEN(Position, POSITIONLIST)
-STRUCT_GEN(Health, HEALTHLIST)
-
-#undef to_array_field
-
-#define to_array_field(TYPE, NAME)                                 \
-  TYPE NAME[MAX_ENTITY_AMOUNT];                                    \
-
-
-ARRAY_GEN(Position, POSITIONLIST)
-ARRAY_GEN(Health, HEALTHLIST)
-
-
-#undef to_array_field*/
-
