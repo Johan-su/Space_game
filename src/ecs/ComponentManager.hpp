3e8cf2140283ad67a441753c30d479f784947130
Johan-su <>
2021-12-30 23:02:42 +0100

    moved func implementations into cpp files

--- src/ecs/ComponentManager.hpp
+++ src/ecs/ComponentManager.hpp
@@ -19,7 +19,7 @@ struct Component_data
 {
     uint64_t m_componentIdCount;
 
-    #define STRUCT_GEN(NAME, vargs...) NAME ## _array m_ ## NAME;
+    #define STRUCT_GEN(NAME, vargs...) NAME ## _array *m_ ## NAME;
     #define DATA_GEN(TYPE, VAR)    
 
     COMPONENT_LIST(STRUCT_GEN, DATA_GEN)
@@ -33,8 +33,8 @@ struct Component_data
 
 namespace Component_functions 
 {
-    bool init(Memory_pool *mem, Component_data *compdata);
-    bool clean(Memory_pool *mem, Component_data *compdata);
+    bool init(Memory_pool *mm, Component_data *compdata);
+    bool clean(Memory_pool *mm, Component_data *compdata);
     uint64_t getId(Component_data *cdata);
 
     template<typename T>
@@ -45,13 +45,13 @@ namespace Component_functions
     }
 
     template <typename T>
-    void set_component(Component_data *cdata, Entity e, T& comp);// { assert(false, "non specialized template"); }
+    void set_component(Component_data *cdata, Entity e, T& comp);
 
     template<typename T>
-    void destroy_component(Component_data *cdata, Entity e) { assert(false, "non specialized template"); }
+    void destroy_component(Component_data *cdata, Entity e);
 
     template <typename T>
-    T get_component(Component_data *cdata, Entity e) { assert(false, "non specialized template"); return nullptr; }
+    T get_view(Component_data *cdata, Entity e);
 
     template<typename T>
     Signature get_component_signature(Component_data *cdata)
