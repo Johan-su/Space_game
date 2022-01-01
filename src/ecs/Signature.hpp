92742acf985f574868b424565dc9099af4128782
Johan-su <jsbollll@gmail.com>
2022-01-01 20:20:18 +0100

    typedef Signature

--- src/ecs/Signature.hpp
+++ src/ecs/Signature.hpp
@@ -2,7 +2,5 @@
 #include "ecs_constants.hpp"
 #include <cstdint>
 static_assert(MAX_COMPONENT_TYPES <= (sizeof(uint64_t) * 8), "Signatures cannot match max amount of components");
-struct Signature
-{
-    uint64_t sigdata;
-};
+
+typedef uint64_t Signature;
