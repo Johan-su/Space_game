a2d6368600e5351412d0d729abf7b9094ed8a823
Johan-su <jsbollll@gmail.com>
2022-01-01 20:22:21 +0100

    added get_view for health_system

--- src/ecs/systems/health_system.cpp
+++ src/ecs/systems/health_system.cpp
@@ -1,5 +1,6 @@
 #include "../Entity.hpp"
 #include "../Signature.hpp"
+#include "../View_Groups.hpp"
 #include "../EntityManager.hpp"
 #include "../ComponentManager.hpp"
 #include "health_system.hpp"
@@ -16,5 +17,5 @@ void health_system::clean()
 
 void health_system::update(Component_data *cdata, Entity_data *edata, Signature sig)
 {
-    auto& data = Component_functions::get_view<Health_component>(cdata);
+    auto & data = Component_functions::get_view<Health_component>(cdata);
 }
