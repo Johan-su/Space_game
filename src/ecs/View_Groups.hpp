fdf98058da39dfa6c39a03819e1888d9e3686e32
Johan-su <jsbollll@gmail.com>
2022-01-01 21:04:02 +0100

    fixed header problems

--- src/ecs/View_Groups.hpp
+++ src/ecs/View_Groups.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "Entity.hpp"
-#include "ComponentManager.hpp"
+#include "Components.hpp"
+#include "component_arrays.hpp"
 
 struct BaseCollection 
 {
@@ -8,9 +9,8 @@ struct BaseCollection
 };
 
 template <typename T>
-struct View// : public BaseCollection
+struct View : public BaseCollection
 {
-    T p[12];
     //only the template specializations should be used.
 };
 
