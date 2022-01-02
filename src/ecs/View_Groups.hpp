35cd3c14ac799967630861d2cb9e8024058ce696
Johan-su <jsbollll@gmail.com>
2022-01-02 21:36:51 +0100

    macro creating groups for every component

--- src/ecs/View_Groups.hpp
+++ src/ecs/View_Groups.hpp
@@ -6,6 +6,7 @@
 struct BaseCollection 
 {
     Entity m_entities[MAX_ENTITY_AMOUNT];
+    uint64_t size = 0;
 };
 
 template <typename T>
@@ -13,6 +14,18 @@ struct View : public BaseCollection
 {
     //only the template specializations should be used.
 };
+#define STRUCT_GEN(NAME, vargs...) \
+template<> \
+struct View<NAME ## _component> : public BaseCollection {vargs}; \
+
+#define DATA_GEN(TYPE, VAR) \
+TYPE VAR[MAX_ENTITY_AMOUNT];
+
+COMPONENT_LIST(STRUCT_GEN, DATA_GEN)
+
+#undef STRUCT_GEN
+#undef DATA_GEN
+
 
 template <typename T>
 struct PartialView : public BaseCollection
