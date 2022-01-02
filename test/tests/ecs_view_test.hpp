ec2810fe928172633136d8d2901ad7b1f85a7737
Johan-su <jsbollll@gmail.com>
2022-01-02 21:37:39 +0100

    begun adding view_test

---
+++ test/tests/ecs_view_test.hpp
@@ -0,0 +1,32 @@
+#include "../../src/ecs/ecs.hpp"
+
+
+namespace ecs_view_test
+{
+    class health_system : System
+    {
+        void update(Registry_data *rdata)
+        {
+            auto & view = Registry_functions::get_view<Health_component>(rdata, m_Systemsignature);
+            //for(auto& tuple ) /TODO(johan): finish test
+        }
+    };
+    int test()
+    {
+        auto *reg = new Registry_data();
+        Registry_functions::init(reg);
+
+
+
+
+
+
+
+
+
+        Registry_functions::clean(reg);
+        delete reg;
+        reg = nullptr;
+        return 0;
+    }
+}
