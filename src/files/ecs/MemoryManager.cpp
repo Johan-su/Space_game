4ada38f94b965446e0e398705970a7f10096fff3
Johan-su <jsbollll@gmail.com>
2022-01-10 23:12:58 +0100

    moved alloc, dealloc to header

--- src/files/ecs/MemoryManager.cpp
+++ src/files/ecs/MemoryManager.cpp
@@ -52,38 +52,4 @@ void Memory::dump(Memory_pool *mm, const size_t size = 512, bool addr = true)
     }
 }
 
-
-template <typename T>  
-T *Memory::alloc(Memory_pool *mm, size_t amount) //TODO(johan) fix, memory alignment
-{
-    assert(amount > 0, "Cannot allocate 0 bytes");
-    assert(mm->m_MemoryActive, "Inactive memory pool");
-    const size_t type_size = sizeof(T);
-    assert(type_size * amount + mm->m_bytesAllocated < MEMORY_POOL_SIZE, "Out of memory");
-  
-    uint64_t tmp = mm->m_bytesAllocated;
-    mm->m_bytesAllocated += type_size * amount;
-    
-    return (T*)(((char*)(mm->m_runTimeData))+ tmp); // C++ forcing this mess
-}
-
-
-
-template <typename T>
-void Memory::dealloc(Memory_pool *mm, T *pointer, size_t amount)
-{
-    assert(amount > 0, "Cannot deallocate 0 bytes");
-    assert(mm->m_MemoryActive, "Inactive memory pool");
-    const size_t size = sizeof(T);
-    
-    assert(mm->m_bytesAllocated - size * amount > 0, "Deallocation outside memory pool");
-    
-    assert(pointer >= mm->m_runTimeData, "Pointer outside memory pool");
-    assert((char*)pointer < (char*)mm->m_runTimeData + MEMORY_POOL_SIZE, "Pointer outside memory pool");
-  
-    for(size_t i = 0; i < size * amount; ++i)
-    {
-        *((char*)(pointer) + i) = 0;
-    }
-}
   
