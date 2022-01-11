722ab3bf12b3e6381a52d31440ba369ffff432c2
Johan-su <jsbollll@gmail.com>
2022-01-11 23:34:41 +0100

    aligned `\`

--- src/files/ecs/ecs_assert.hpp
+++ src/files/ecs/ecs_assert.hpp
@@ -1,15 +1,16 @@
 #pragma once
 #include <iostream>
 #ifdef _DEBUG
-#define assert(condition, message) \
- do \
-{ \
-   if(!(condition)) \
-   { \
-      std::cerr << "Assertion " << "[ " #condition " ]" \
+#define assert(condition, message)                         \
+ do                                                        \
+{                                                          \
+   if(!(condition))                                        \
+   {                                                       \
+      std::cerr << "Assertion " << "[ " #condition " ]"    \
       << " failed in " << __FILE__ << " line " << __LINE__ \
-      << ": " << message << "\n"; exit(1); \
-   } \
+      << ": " << message << "\n";                          \
+      exit(1);                                             \
+   }                                                       \
  } while(0)
 
 #else
