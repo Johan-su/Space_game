fc5d3b41d5778742fc86dbb1ec57dffdbd99a086
Johan <jsbollll@gmail.com>
2022-01-23 01:11:59 +0100

    put static assert after signature definition

--- src/files/ecs/Signature.hpp
+++ src/files/ecs/Signature.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "ecs_constants.hpp"
 #include <cstdint>
-static_assert(MAX_COMPONENT_TYPES <= (sizeof(uint64_t) * 8), "Signatures cannot match max amount of components");
 
 typedef uint64_t Signature;
+
+static_assert(MAX_COMPONENT_TYPES <= (sizeof(Signature) * 8), "Signatures cannot match max amount of components");
