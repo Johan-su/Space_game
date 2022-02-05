fac973f66c6b5ea375ac2bd303da2c8473b7cf0b
Johan <jsbollll@gmail.com>
2022-02-05 01:52:45 +0100

    member amount in struct introspection

--- src/files/ecs/Components.hpp
+++ src/files/ecs/Components.hpp
@@ -3,7 +3,7 @@
 #include "ecs_constants.hpp"
 #include "ecs_assert.hpp"
 
-//#define _DEBUG
+
 #define COMPONENT_LIST(STRC, DATA) \
 STRC(Position,                     \
 DATA(float, x)                     \
@@ -17,7 +17,13 @@ STRC(Health,                       \
 DATA(float, hp)                    \
 DATA(float, hp_regen)              \
 )                                  \
-
+STRC(Test,                         \
+DATA(float, a)                     \
+DATA(float, b)                     \
+DATA(int, c)                       \
+DATA(bool, d)                      \
+DATA(short, e)                     \
+)
 
 // struct component generator
 #define STRUCT_GEN(NAME, vargs...) \
@@ -31,3 +37,32 @@ COMPONENT_LIST(STRUCT_GEN, DATA_GEN)
 
 #undef STRUCT_GEN
 #undef DATA_GEN
+
+//######## introspection ############
+
+// Member amount
+#define STRUCT_GEN(NAME, vargs...) \
+const size_t NAME ## _MEMBER_AMOUNT = 0 vargs; // expands to  0 ( + 1 + 1 + 1 + ... 1; ) 
+
+#define DATA_GEN(TYPE, VAR) \
++ 1
+
+COMPONENT_LIST(STRUCT_GEN, DATA_GEN)
+
+#undef STRUCT_GEN
+#undef DATA_GEN
+
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
