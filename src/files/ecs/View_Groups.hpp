e9b10f883b39b774be2ab0af58681aca58ec6238
Johan <jsbollll@gmail.com>
2022-02-05 15:54:28 +0100

    removal of views and begun on Collection

--- src/files/ecs/View_Groups.hpp
+++ src/files/ecs/View_Groups.hpp
@@ -3,38 +3,9 @@
 #include "Components.hpp"
 #include "component_arrays.hpp"
 
-struct BaseCollection 
+struct Collection 
 {
     Entity m_entities[MAX_ENTITY_AMOUNT];
+    void *comparray;
     uint64_t size = 0;
-};
-
-template <typename T>
-struct View : public BaseCollection
-{
-    //only the template specializations should be used.
-};
-#define STRUCT_GEN(NAME, vargs...) \
-template<> \
-struct View<NAME ## _component> : public BaseCollection {vargs}; \
-
-#define DATA_GEN(TYPE, VAR) \
-TYPE VAR[MAX_ENTITY_AMOUNT];
-
-COMPONENT_LIST(STRUCT_GEN, DATA_GEN)
-
-#undef STRUCT_GEN
-#undef DATA_GEN
-
-
-template <typename T>
-struct PartialView : public BaseCollection
-{
-    //only the template specializations should be used.
-};
-
-template <typename T>
-struct Group : public BaseCollection
-{
-    //only the template specializations should be used.
 };
