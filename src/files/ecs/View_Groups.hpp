18c2c36dae28134c25c9683649350123d4b6bb5f
Johan <jsbollll@gmail.com>
2022-02-11 00:16:10 +0100

    needs to be thought through

--- src/files/ecs/View_Groups.hpp
+++ src/files/ecs/View_Groups.hpp
@@ -12,9 +12,48 @@ struct Collection
 
 
 
-#define get_collection(vargs...) _get_collection(#vargs)
+#define get_collection(first, vargs...) _get_collection(first, vargs)
+
+
+inline constexpr bool equal(const char *lhs, const char *rhs)
+{
+    while (*lhs || *rhs)
+    {
+        if (*lhs++ != *rhs++)
+        {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+inline constexpr void component_to_enum(const char *component, size_t *val)
+{
+    #define STR(s1) #s1  
+
+
+    #define STRUCT_GEN(NAME, vargs...)            \
+    if(equal(component, STR(NAME ## _component))) \
+    {                                             \
+        *val = NAME ##_CONSTANT;                  \
+    }
+
+    #define DATA_GEN(TYPE, VAR)
+
+    COMPONENT_LIST(STRUCT_GEN, DATA_GEN)
+
+
+    #undef STRUCT_GEN
+    #undef DATA_GEN
+    #undef STR
+    
+}
+
 
 inline Collection _get_collection(const char *components)
 {
     printf("%s", components);
+
+    return *(Collection*)(0);
 }
