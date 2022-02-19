f4d779882345b54927d64610c0af41b6a35c7a08
Johan <jsbollll@gmail.com>
2022-02-19 03:10:43 +0100

    removed complicated mess

--- src/files/ecs/View_Groups.hpp
+++ src/files/ecs/View_Groups.hpp
@@ -5,55 +5,7 @@
 
 struct Collection 
 {
-    // Entity m_entities[MAX_ENTITY_AMOUNT];
+    Entity *m_entities;
     void *comparray; // only one component type
-    uint64_t size = 0;
+    size_t size;
 };
-
-
-
-#define get_collection(first, vargs...) _get_collection(first, vargs)
-
-
-inline constexpr bool equal(const char *lhs, const char *rhs)
-{
-    while (*lhs || *rhs)
-    {
-        if (*lhs++ != *rhs++)
-        {
-            return false;
-        }
-
-    }
-    return true;
-}
-
-inline constexpr void component_to_enum(const char *component, size_t *val)
-{
-    #define STR(s1) #s1  
-
-
-    #define STRUCT_GEN(NAME, vargs...)            \
-    if(equal(component, STR(NAME ## _component))) \
-    {                                             \
-        *val = NAME ##_CONSTANT;                  \
-    }
-
-    #define DATA_GEN(TYPE, VAR)
-
-    COMPONENT_LIST(STRUCT_GEN, DATA_GEN)
-
-
-    #undef STRUCT_GEN
-    #undef DATA_GEN
-    #undef STR
-    
-}
-
-
-inline Collection _get_collection(const char *components)
-{
-    printf("%s", components);
-
-    return *(Collection*)(0);
-}
