714294974c7711112f4df2e1f2f826f4dd5a5f2e
Johan <jsbollll@gmail.com>
2022-02-19 22:08:27 +0100

    Collection, constructor, destructor

--- src/files/ecs/View_Groups.hpp
+++ src/files/ecs/View_Groups.hpp
@@ -4,9 +4,26 @@
 #include "component_arrays.hpp"
 
 template<typename T>
-struct Collection
+class Collection
 {
+public:
     size_t size;
     Entity *m_entities;
     T *comparray; // only one component type
+
+    Collection(size_t size)
+    {
+        this->size = size;
+
+        this->m_entities = Memory::alloc<Entity>(mm, size);
+        this->comparray  = Memory::alloc<T>(mm, size);
+    }
+
+
+
+    ~Collection()
+    {
+        Memory::dealloc<Entity>(mm, m_entities, size);
+        Memory::dealloc<T>(mm, comparray, size);
+    }
 };
