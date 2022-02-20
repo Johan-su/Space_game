5d29a8a3f8e0f3f6748f0138aedc5cba4e106817
Johan <jsbollll@gmail.com>
2022-02-20 22:31:46 +0100

    initalizing arrays

--- src/files/ecs/ComponentManager.cpp
+++ src/files/ecs/ComponentManager.cpp
@@ -6,12 +6,20 @@
 
 #include "string.h"
 
+
+
+
 bool Component_functions::init(Memory_pool *mm, Component_data *cdata)
 {
-    cdata->m_componentTypesCount = 0;
+    memset(cdata->m_array_init, 0, sizeof(cdata->m_array_init[0]) * MAX_COMPONENT_TYPES);
     memset(cdata->m_array_sizes, 0, sizeof(cdata->m_array_sizes[0]) * MAX_COMPONENT_TYPES);
+    memset(cdata->m_component_alignments, 0, sizeof(cdata->m_component_alignments[0]) * MAX_COMPONENT_TYPES);
+    memset(cdata->m_component_sizes, 0, sizeof(cdata->m_component_sizes[0]) * MAX_COMPONENT_TYPES);
+    memset(cdata->m_componentArrays, 0, sizeof(cdata->m_componentArrays[0]) * MAX_COMPONENT_TYPES);
+    cdata->m_componentTypesCount = 0;
 
-    memset(cdata->m_array_init, 0, sizeof(bool) * MAX_COMPONENT_TYPES);
+
+
 
     return 0;
 }
@@ -31,8 +39,36 @@ bool Component_functions::clean(Memory_pool *mm, Component_data *cdata)
 }
 
 
-void Component_functions::destroy_entity(Component_data *cdata, Entity e) 
+void Component_functions::destroy_entity(Component_data *cdata, Entity e)
 {
+    assert(e < MAX_ENTITY_AMOUNT - 1, "entity id out of bounds");
+
+    for(size_t i = 0; i < cdata->m_componentTypesCount; ++i)
+    {
+        assert(cdata->m_array_init[i], "disordered/uninitalized componentArrays");
+
+        void *comparray      = cdata->m_componentArrays[i];
+        size_t compsize      = cdata->m_component_sizes[i];
+        size_t size          = *(size_t*)(comparray);
+        Entity *sparse_array = (Entity*)((size_t*)(comparray) + 1);
+        Entity *entity_list  = (Entity*)((size_t*)(comparray) + 1) + 1;
+        char *dense_array    = (char*)((Entity*)((size_t*)(comparray) + 1) + 2);
+
+
+        assert(size > 0, "array size is 0");
+
+        Entity laste = entity_list[size - 1];
+
+        memcpy( static_cast<void*>(&(dense_array[sparse_array[e] * compsize])), // high risk of segfault/exception
+                static_cast<void*>(&(dense_array[sparse_array[laste] * compsize])), 
+                compsize);
+
+        sparse_array[laste] = sparse_array[e];
+        
+        sparse_array[e] = ENTITY_NULL;
+        --size;
+    }
+
 }
 
 size_t Component_functions::get_id()
