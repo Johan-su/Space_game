b6c626d687707a29d61a856afbbf182b1b404188
Johan <jsbollll@gmail.com>
2022-02-25 23:15:32 +0100

    clarification on earlier comment

--- src/files/ecs/Components.hpp
+++ src/files/ecs/Components.hpp
@@ -32,7 +32,7 @@ struct Health_regen
 
 
 template<typename T>
-struct ComponentArray //changing variable order will affect componentManager functions
+struct ComponentArray //changing variables or order will affect componentManager functions
 {
     size_t size;
     Entity sparse_array[MAX_ENTITY_AMOUNT];
