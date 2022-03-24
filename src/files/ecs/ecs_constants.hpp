d6695792c4d70b084b5b8f5b82e3d914036e95cb
Johan <jsbollll@gmail.com>
2022-03-25 00:24:36 +0100

    added MAX_EVENT_TYPES

--- src/files/ecs/ecs_constants.hpp
+++ src/files/ecs/ecs_constants.hpp
@@ -1,6 +1,12 @@
 #pragma once
 #define MEMORY_POOL_SIZE 104857600 // 100 MiB
-#define MAX_ENTITY_AMOUNT 1000000 // 2^10
+
+
+#define MAX_ENTITY_AMOUNT 1000000 // about 2^10
 #define ENTITY_NULL MAX_ENTITY_AMOUNT - 1
+
+
 #define MAX_COMPONENT_TYPES 256
 #define VIEW_SIZE 4096 // increase for faster iteration at the cost of higher memory usage.
+
+#define MAX_EVENT_TYPES 256
