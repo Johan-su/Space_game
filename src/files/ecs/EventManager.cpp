31857fb00f7fa53fc6b3d689dbfb42e4663203de
Johan <jsbollll@gmail.com>
2022-03-26 00:20:11 +0100

    simplified eventManager

--- src/files/ecs/EventManager.cpp
+++ src/files/ecs/EventManager.cpp
@@ -1,15 +1,14 @@
 #include "EventManager.hpp"
 #include "MemoryManager.hpp"
 
-void Event_functions::init(event_data *ed)
+void Event_functions::init(event_data *ed, void (event_listener)(size_t))
 {
-    memset(ed->event_arrays,     0, sizeof(ed->event_arrays[0])     * MAX_EVENT_TYPES);
-    memset(ed->event_sizes,      0, sizeof(ed->event_sizes[0])      * MAX_EVENT_TYPES);
-    memset(ed->event_alignments, 0, sizeof(ed->event_alignments[0]) * MAX_EVENT_TYPES);
-    memset(ed->array_init,       0, sizeof(ed->array_init[0])       * MAX_EVENT_TYPES);
+    memset(ed->events,      0, sizeof(ed->events[0])      * MAX_EVENT_TYPES);
+    memset(ed->event_sizes, 0, sizeof(ed->event_sizes[0]) * MAX_EVENT_TYPES);
 
 
     ed->event_types_count = 0;
+    ed->event_listener = event_listener;
 
 }
 
@@ -18,9 +17,9 @@ void Event_functions::clean(Memory_pool *mm, event_data *ed)
 {
     for(size_t i = 0; i < ed->event_types_count; ++i)
     {
-        if(ed->array_init[i])
+        if(ed->event_sizes[i] > 0)
         {
-            Memory::dealloc(mm, (char*)(ed->event_arrays + i), ed->event_sizes[i] * MAX_EVENT_TYPES);
+            Memory::dealloc(mm, (char*)(ed->events + i), ed->event_sizes[i]);
         }
     }
 }
--- src/files/ecs/EventManager.hpp
+++ src/files/ecs/EventManager.hpp
@@ -5,33 +5,23 @@
 
 
 
-template<typename T>
-struct event_array
-{
-    T eventlist[EVENT_QUEUE_SIZE];
-    bool activelist[EVENT_QUEUE_SIZE];
-    size_t queue_start_index;
-    size_t size;
-
-};
 
 
 struct event_data
 {
-    void *event_arrays[MAX_EVENT_TYPES];
+    void *events[MAX_EVENT_TYPES];
 
     size_t event_sizes[MAX_EVENT_TYPES];
-    size_t event_alignments[MAX_EVENT_TYPES];
-
     size_t event_types_count;
 
-    bool array_init[MAX_EVENT_TYPES];
+    void (*event_listener)(size_t);
+
 };
 
 
 namespace Event_functions
 {
-    void init(event_data *ed);
+    void init(event_data *ed, void (event_listener)(size_t));
     void clean(Memory_pool *mm, event_data *ed);
 
 }
@@ -46,30 +36,29 @@ namespace Event_functions
     }
 
 
-
-
     template<typename T>
-    void init_event(event_data *ed)
+    void init_event(Memory_pool *mm, event_data *ed)
     {
         const size_t eventid = get_unique_event_id<T>(ed);
 
-        event_array<T> *eventarray = (event_array<T>*)ed->event_arrays[eventid];
-        eventarray = Memory::alloc<event_array<T>>(mm);
+        T *event = (T*)ed->events[eventid];
+        event = Memory::alloc<T>(mm);
 
-        eventarray->size = 0;
-        eventarray->queue_start_index = 0;
-
-        for(size_t i = 0; i < EVENT_QUEUE_SIZE; ++i)
-        {
-            eventarray->activelist[i] = false; 
-        }
-
-        ed->event_sizes[eventid]      = sizeof(T);
-        ed->event_alignments[eventid] = alignof(T);
-        ed->array_init[eventid]       = true;
+        ed->event_sizes[eventid] = sizeof(T);
 
 
-        dbg(std::cout << "sizeof " << typeid(T).name() << "_array : " << sizeof(event_array<T>) << "\n");
+        dbg(std::cout << "sizeof " << typeid(T).name() << "_event : " << sizeof(T) << "\n");
     }
+
+
+    template<typename T>
+    void broadcast_event(event_data *ed, T event)
+    {
+        const size_t eventid = get_unique_event_id<T>();
+        T *event = (T*)ed->events[eventid];
+
+        (*ed->event_listener)(eventid);
+    }
+
 
 }
