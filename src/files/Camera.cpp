b793fda4eb2d73c2b48abfd6a85db69eb3b231dd
Johan <jsbollll@gmail.com>
2022-03-29 18:22:18 +0200

    implemented basic camera

---
+++ src/files/Camera.cpp
@@ -0,0 +1,49 @@
+#include "Camera.hpp"
+
+
+void Camera_functions::init(Camera *camera, uint16_t screen_width, uint16_t screen_height)
+{
+    camera->world_x = 0.0f;
+    camera->world_y = 0.0f;
+
+    camera->screen_width = screen_width;
+    camera->screen_height = screen_height;
+
+    camera->world_width = (float)screen_width;
+    camera->world_height = (float)screen_height;
+
+}
+
+
+void Camera_functions::clean(Camera *camera)
+{
+    return;
+}
+
+
+float Camera_functions::screen_to_world_x(Camera *camera, uint16_t x)
+{
+    float worldunit_per_pixel = camera->world_width / SCREEN_WIDTH;
+
+    float result = camera->world_x + worldunit_per_pixel * x;
+
+    return result;
+    
+}
+
+
+float Camera_functions::screen_to_world_y(Camera *camera, uint16_t y)
+{
+    float worldunit_per_pixel = camera->world_height / SCREEN_HEIGHT;
+
+    float result = camera->world_y + worldunit_per_pixel * y;
+
+    return result;
+}
+
+
+void Camera_functions::set_camera_center(Camera *camera, float x, float y)
+{
+    camera->world_x = x - (camera->screen_width / 2);
+    camera->world_y = y - (camera->screen_height / 2);
+}
---
+++ src/files/Camera.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+
+#include <stdint.h>
+
+struct Camera
+{
+    float world_x;
+    float world_y;
+
+    float world_width;
+    float world_height;
+
+    uint16_t screen_width;
+    uint16_t screen_height;
+    
+};
+
+namespace Camera_functions
+{
+    void init(Camera *camera, uint16_t screen_width, uint16_t screen_height);
+    void clean(Camera *camera);
+
+    float screen_to_world_x(Camera *camera, uint16_t x);
+    float screen_to_world_y(Camera *camera, uint16_t y);
+
+    void set_camera_center(Camera *camera, float x, float y);
+}
