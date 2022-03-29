facd39097fc70157298c44f4b1347d3ed63ac8fc
Johan <jsbollll@gmail.com>
2022-03-29 19:13:07 +0200

    setup_game_state

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -8,6 +8,7 @@ int main(int argv, char *args[])
     game_data *game = Game::create_game();
 
     Game::init(game);
+    Game::setup_game_state(game);
     Game::run(game);
     Game::clean(game);
 
