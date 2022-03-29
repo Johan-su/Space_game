f7d6409cb3edbecffe5669e0318228e89811c212
Johan <jsbollll@gmail.com>
2022-03-29 23:24:29 +0200

    added Tex_sprite to load images from atlases

--- src/files/Texture.cpp
+++ src/files/Texture.cpp
@@ -6,9 +6,13 @@
 #include <stdio.h>
 void Texture::init(textures_data *td)
 {
-    memset(td->enum_to_texture, 0xFF, sizeof(td->enum_to_texture[0]) * MAX_TEXTURE_TYPES);
     memset(td->texture_list, 0, sizeof(td->texture_list[0]) * MAX_TEXTURE_TYPES);
+    memset(td->enum_to_texture, 0xFF, sizeof(td->enum_to_texture[0]) * MAX_TEXTURE_TYPES);
+
+    memset(td->sprite_list, 0xFF, sizeof(td->sprite_list[0]) * MAX_SPRITE_TYPES);
+    memset(td->enum_to_sprite, 0xFF, sizeof(td->enum_to_sprite[0]) * MAX_SPRITE_TYPES);
     td->texture_count = 0;
+    td->sprite_count = 0;
 }
 
 
@@ -45,10 +49,38 @@ void Texture::load_texture(SDL_Renderer *renderer, textures_data *td, uint32_t e
 }
 
 
-SDL_Texture *Texture::get_texture(textures_data *td, uint32_t enum_id)
+void Texture::init_sprite(textures_data *td, uint32_t sprite_id, uint32_t texture_id, uint32_t x, uint32_t y, uint32_t w, uint32_t h)
 {
-    assert(enum_id >= MAX_TEXTURE_TYPES, "enum_id cannot be greater than MAX_TEXTURE TYPES");
-    return td->texture_list[td->enum_to_texture[enum_id]];
+    assert(sprite_id <= MAX_SPRITE_TYPES, "sprite id to big");
+
+    td->enum_to_sprite[sprite_id] = td->sprite_count;
+
+    assert(texture_id <= MAX_TEXTURE_TYPES, "texture id to big");
+
+    Tex_Sprite sprite = Tex_Sprite();
+
+    sprite.x = x;
+    sprite.y = y;
+    sprite.w = w;
+    sprite.h = h;
+
+    sprite.texture_index = texture_id;
+
+    td->sprite_list[td->sprite_count] = sprite;
+
 }
+
+SDL_Texture *Texture::get_texture(textures_data *td, uint32_t id)
+{
+    assert(id <= MAX_TEXTURE_TYPES, "id cannot be greater than MAX_TEXTURE_TYPES");
+    return td->texture_list[td->enum_to_texture[id]];
+}
+
+Tex_Sprite *Texture::get_sprite(textures_data *td, uint32_t id)
+{
+    assert(id <= MAX_SPRITE_TYPES, "id cannot be greater than MAX_SPRITE_TYPES");
+    return &td->sprite_list[td->enum_to_sprite[id]];
+}
+
 
 
--- src/files/Texture.hpp
+++ src/files/Texture.hpp
@@ -4,19 +4,38 @@
 #include <stdint.h>
 
 #define MAX_TEXTURE_TYPES 512
+#define MAX_SPRITE_TYPES 512
 
 enum Texture_id_map
 {
+    SHIP_texture
+};
+
+enum Sprite_id_map
+{
     SHIP1, SHIP2
+};
+
+
+struct Tex_Sprite
+{
+    uint32_t x;
+    uint32_t y;
+    uint32_t w;
+    uint32_t h;
+
+    uint32_t texture_index;
 };
 
 struct textures_data
 {
     SDL_Texture *texture_list[MAX_TEXTURE_TYPES]; //TODO(johan) maybe change to linked list
-    
-    uint32_t enum_to_texture[MAX_TEXTURE_TYPES];
-
+    uint32_t enum_to_texture[MAX_SPRITE_TYPES];
     size_t texture_count;
+    
+    Tex_Sprite sprite_list[MAX_SPRITE_TYPES];
+    uint32_t enum_to_sprite[MAX_SPRITE_TYPES];
+    size_t sprite_count;
 };
 
 
@@ -25,8 +44,13 @@ namespace Texture
     void init(textures_data *td);
     void clean(textures_data *td);
 
-    void load_texture(SDL_Renderer *renderer, textures_data *td, uint32_t enum_id, const char *path);
-    SDL_Texture *get_texture(textures_data *td, uint32_t enum_id);
+    void load_texture(SDL_Renderer *renderer, textures_data *td, uint32_t id, const char *path);
+
+    void init_sprite(textures_data *td, uint32_t sprite_id, uint32_t texture_id, uint32_t x, uint32_t y, uint32_t w, uint32_t h);
+
+    SDL_Texture *get_texture(textures_data *td, uint32_t id);
+
+    Tex_Sprite *get_sprite(textures_data *td, uint32_t id);
 
 
 
