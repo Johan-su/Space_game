63ee546a8c3e3dc658add108f316154258023734
Johan <jsbollll@gmail.com>
2022-03-29 15:52:57 +0200

    separate assert for game

---
+++ src/files/assert.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+
+#ifdef _DEBUG
+#define assert(condition, message)                         \
+do                                                         \
+{                                                          \
+   if(!(condition))                                        \
+   {                                                       \
+      std::cerr << "Assertion " << "[ " #condition " ]"    \
+      << " failed in " << __FILE__ << " line " << __LINE__ \
+      << ": " << message << "\n";                          \
+      exit(1);                                             \
+   }                                                       \
+} while(0)
+
+#define dbg(expression) \
+do                      \
+{                       \
+   expression;          \
+} while (0) 
+
+
+
+
+#else
+#define assert(condition, message) \
+do {} while(0)
+
+#define dbg(expression) \
+do {} while(0)
+
+
+ #endif
