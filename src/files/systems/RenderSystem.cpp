0faa88d86e9b532ca61390e3f58990fd8d6672c6
Johan <jsbollll@gmail.com>
2022-04-02 19:50:51 +0200

    made function more clear

--- src/files/systems/RenderSystem.cpp
+++ src/files/systems/RenderSystem.cpp
@@ -1,8 +1,8 @@
 #include "RenderSystem.hpp"
-#include "Components_Events.hpp"
-#include "assert.hpp"
+#include "../Components_Events.hpp"
+#include "../assert.hpp"
 
-#include "ecs/ecs.hpp"
+#include "../ecs/ecs.hpp"
 
 #include <SDL.h>
 #include <stdint.h>
@@ -16,10 +16,11 @@ void RenderSystem::render(game_data *game)
     auto angle_view  = Registry_functions::get_view<Angle, Sprite, Position, Size>(game->registry);
     auto sprite_view = Registry_functions::get_view<Sprite, Position, Size, Angle>(game->registry);
 
-    for(size_t i = 0; i < pos_view.size; ++i) //TODO(Johan) change to treat posx, posy as center vars
+    for(size_t i = 0; i < pos_view.size; ++i)
     {
         //dbg(printf("running in render for loop\n"));
         Tex_Sprite *sprite   = Texture::get_sprite(game->texture, sprite_view.comparray[i].texture_id);
+        SDL_Texture *texture = Texture::get_texture(game->texture, sprite->texture_index);
 
         SDL_Rect srcrect     = SDL_Rect{(int)sprite->x, (int)sprite->y, (int)sprite->w, (int)sprite->h};
 
@@ -27,12 +28,11 @@ void RenderSystem::render(game_data *game)
         auto size = size_view.comparray[i];
 
 
-        SDL_FRect dstrect    = SDL_FRect{(pos.x - size.width / 2 - camera->world_x) * camera->world_scale_x,
-                                         (pos.y - size.height / 2 - camera->world_y) * camera->world_scale_y, 
+        SDL_FRect dstrect    = SDL_FRect{(pos.x - (size.width / 2) - camera->world_x) * camera->world_scale_x,
+                                         (pos.y - (size.height / 2) - camera->world_y) * camera->world_scale_y, 
                                          size.width * camera->world_scale_x, 
                                          size.height * camera->world_scale_y};
         
-        SDL_Texture *texture = Texture::get_texture(game->texture, sprite->texture_index);
 
         SDL_RenderCopyExF(game->renderer, texture, &srcrect, &dstrect, angle_view.comparray[i].angle, NULL, SDL_FLIP_NONE);
     }
--- src/files/systems/RenderSystem.hpp
+++ src/files/systems/RenderSystem.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "Game.hpp"
+#include "../Game.hpp"
 
 namespace RenderSystem
 {
