bb84218d197a2fce03560d90d71d1ad982ead13e
Johan <jsbollll@gmail.com>
2022-06-21 18:45:32 +0200

    better coding practices

--- src/Game/systems/AngleSystem.cpp
+++ src/Game/systems/AngleSystem.cpp
@@ -1,16 +1,13 @@
 #include "AngleSystem.hpp"
 #include "../Components_Events.hpp"
 
-#include <stdlib.h>
-
-
 
 void AngleSystem::update(Ecs::Registry *registry, float Ts)
 {
     Application::clear_view_buffer();
-    auto angle_vel_view = Ecs::get_view<AnglularVelocity, Angle>(registry);
+    View<AnglularVelocity> *angle_vel_view = Ecs::get_view<AnglularVelocity, Angle>(registry);
 
-    for(size_t i = 0; i < angle_vel_view->size; ++i)
+    for (size_t i = 0; i < angle_vel_view->size; ++i)
     {
         Entity e = angle_vel_view->entity_list[i];
         Angle *ang = Ecs::get_component<Angle>(registry, e);
@@ -18,11 +15,11 @@ void AngleSystem::update(Ecs::Registry *registry, float Ts)
 
         ang->angle += ang_vel.angleV * Ts;
 
-        if(ang->angle > 6.28318530708f) // 2pi
+        if (ang->angle > 6.28318530708f) // 2pi
         {
             ang->angle -= 6.28318530708;
         } 
-        else if(ang->angle < -6.28318530708f)
+        else if (ang->angle < -6.28318530708f)
         {
             ang->angle += 6.28318530708;  
         }
