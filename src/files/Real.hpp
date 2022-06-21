98d798c6cb6c3756adb886aa2b355542f6ddda92
Johan <jsbollll@gmail.com>
2022-06-21 17:16:01 +0200

    add compile fail if not 64bit

--- src/files/Real.hpp
+++ src/files/Real.hpp
@@ -1,5 +1,12 @@
 #pragma once
+
+#if defined(_WIN64) || defined(__linux__)
+
 #include "ecs/ecs.hpp"
 #include "Application/Application.hpp"
 #include "input/Input.hpp"
 
+#else
+#error Compiles only on 64 bit Windows or Linux
+
+#endif
