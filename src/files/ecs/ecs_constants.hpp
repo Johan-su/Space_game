e2e877b910bbc410d2b4add238ab9b78cae35f65
Johan <jsbollll@gmail.com>
2022-07-11 21:38:32 +0200

    changed macros to constants

--- src/files/ecs/ecs_constants.hpp
+++ src/files/ecs/ecs_constants.hpp
@@ -3,14 +3,15 @@
 #include "../int.hpp"
 
 
-#define ENTITY_NULL MAX_ENTITY_AMOUNT 
 
 
-#define MAX_COMPONENT_TYPES 256
 
-#define PAGE_SIZE 1024 // increase for faster iteration at the cost of higher memory usage.
-#define MAX_PAGE_AMOUNT 4096 //TODO(Johan) swap usage of size and amount
+const Usize PAGE_SIZE = 1024; // increase for faster iteration at the cost of higher memory usage. different from OS pages
+const Usize MAX_PAGE_AMOUNT = 4096; //TODO(Johan) swap usage of size and amount
 
-#define MAX_ENTITY_AMOUNT PAGE_SIZE * MAX_PAGE_AMOUNT // will crash going over MAX_ENTITY_AMOUNT
+const Usize MAX_ENTITY_AMOUNT = PAGE_SIZE * MAX_PAGE_AMOUNT; // will crash going over MAX_ENTITY_AMOUNT
 
-#define MAX_EVENT_TYPES 256
+const Usize ENTITY_NULL = MAX_ENTITY_AMOUNT;
+
+const Usize MAX_COMPONENT_TYPES = 256;
+const Usize MAX_EVENT_TYPES = 256;
