521a6be2e642f2ad5a45ef154bbd2c664f323ee0
Johan <jsbollll@gmail.com>
2022-07-12 21:42:55 +0200

    added max system types constant

--- src/files/ecs/ecs_constants.hpp
+++ src/files/ecs/ecs_constants.hpp
@@ -13,5 +13,7 @@ const Usize MAX_ENTITY_AMOUNT = PAGE_SIZE * MAX_PAGE_AMOUNT; // will crash going
 
 const Usize ENTITY_NULL = MAX_ENTITY_AMOUNT;
 
+
+const Usize MAX_SYSTEM_TYPES    = 256; // per phase, can be more than 256 systems in total
 const Usize MAX_COMPONENT_TYPES = 256;
-const Usize MAX_EVENT_TYPES = 256;
+const Usize MAX_EVENT_TYPES     = 256;
