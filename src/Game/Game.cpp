1bae2aab2b6904ed3ff207ef370c04e991a3ccc9
Johan <jsbollll@gmail.com>
2022-07-12 21:45:02 +0200

    removed all update funcs, changed to ecs pipeline

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -15,67 +15,47 @@
 
 static void init_components(scene *scene)
 {
-    Ecs::init_component<Position>(scene->registry);
-    Ecs::init_component<SizeComponent>(scene->registry);
-    Ecs::init_component<Velocity>(scene->registry);
-    Ecs::init_component<Angle>(scene->registry);
-    Ecs::init_component<AnglularVelocity>(scene->registry);
-    Ecs::init_component<RigidCollision>(scene->registry);
-    Ecs::init_component<Collision>(scene->registry);
-    Ecs::init_component<Player>(scene->registry);
-    Ecs::init_component<SpriteComponent>(scene->registry);
-    Ecs::init_component<MassComponent>(scene->registry);
-    Ecs::init_component<GravityAttractor>(scene->registry);
-    Ecs::init_component<GravityAffected>(scene->registry);
-    Ecs::init_component<Circle_size>(scene->registry);
+    Ecs::init_component<AnglularVelocity>(&scene->registry);
+    Ecs::init_component<Player>(&scene->registry);
+    Ecs::init_component<MassComponent>(&scene->registry);
+    Ecs::init_component<GravityAttractor>(&scene->registry);
+    Ecs::init_component<GravityAffected>(&scene->registry);
 }
 
 
-static void fixed_update(Application_data *app, scene *scene, float Ts)
+static void init_systems(scene *scene)
 {
-
+    Ecs::init_system(&scene->registry, Phase::OnUpdate, CameraController::update);
+    Ecs::init_system(&scene->registry, Phase::OnUpdate, PlayerSystem::update);
+    Ecs::init_system(&scene->registry, Phase::OnUpdate, MovementSystem::update);
+    Ecs::init_system(&scene->registry, Phase::OnUpdate, AngleSystem::update);
+    Ecs::init_system(&scene->registry, Phase::OnUpdate, GravitySystem::update);
+    Ecs::init_system(&scene->registry, Phase::OnUpdate, BoxCollisionSystem::update);
+    Ecs::init_system(&scene->registry, Phase::PostUpdate, RenderSystem::render);
 }
 
 
-
-static void update(Application_data *app, scene *scene, float Ts)
+static void collision_debug(Iter *iter)
 {
-    CameraController::update(scene);
-    MovementSystem::update(scene->registry, Ts);
-    PlayerSystem::update(app, scene->registry, Ts);
-    AngleSystem::update(scene->registry, Ts);
-    GravitySystem::update(scene->registry, Ts);
-    CollisionSystem::update(scene->registry);
-}
-
-
-
-
-static void render(Application_data *app, scene *scene, float Ts)
-{
-    RenderSystem::render(app, scene);
-}
-
-
-static void collision_debug(Ecs::Registry *registry, CollisionEvent *event)
-{
+    CollisionEvent *event = (CollisionEvent *)iter->curr_event;
     printf("collision between [%llu, %llu]", event->e1, event->e2);
 }
 
 
 static void init_events(scene *scene)
 {
-    Ecs::init_event(scene->registry, collision_debug); //TODO(Johan) change to real function pointers
-    Ecs::init_event<void, SpawnEvent>(scene->registry, NULL);
-    Ecs::init_event(scene->registry, EntityCreationSystem::create_planet);
-    Ecs::init_event(scene->registry, EntityCreationSystem::create_player);
+    Ecs::init_event<void, CollisionEvent>(&scene->registry, collision_debug); // TODO(Johan) change to real function pointers
+    Ecs::init_event<void, SpawnEvent>(&scene->registry, NULL);
+    Ecs::init_event<Entity, PlanetSpawnEvent>(&scene->registry, EntityCreationSystem::create_planet);
+    Ecs::init_event<Entity, PlayerSpawnEvent>(&scene->registry, EntityCreationSystem::create_player);
 }
 
 
-static void setup_scene(Application_data *app, scene *scene, const char *pwd)
+static void setup_scene(scene *scene, const char *pwd)
 {
     init_components(scene);
     init_events(scene);
+    init_systems(scene);
 
     
 
@@ -83,27 +63,41 @@ static void setup_scene(Application_data *app, scene *scene, const char *pwd)
     char *planet_path = Application::cat_string(pwd, "/resources/planets/placeholder_planet.bmp");
 
 
-    Application::load_texture(app, SHIP_texture, ship_path);
-    Application::load_texture(app, PLANET_texture, planet_path);
+    Application::load_texture(SHIP_texture, ship_path);
+    Application::load_texture(PLANET_texture, planet_path);
     
-    Application::init_sprite(app, SHIP1, SHIP_texture, 0, 0, 114, 200);
-    Application::init_sprite(app, PLANET1, PLANET_texture, 0, 0, 132, 132);
+    Application::init_sprite(SHIP1, SHIP_texture, 0, 0, 114, 200);
+    Application::init_sprite(PLANET1, PLANET_texture, 0, 0, 132, 132);
 
 
-    Real::set_camera_center(&scene->camera, 0.0f, 0.0f);
+    // Real::set_camera_center(&scene->camera, 0.0f, 0.0f);
 
-    PlayerSpawnEvent pse = PlayerSpawnEvent{0.0f, 0.0f, 114.0f, 200.0f, SHIP1};
-    Entity player = Ecs::broadcast_event<Entity>(scene->registry, &pse);
+    PlayerSpawnEvent pse = {
+        .x         = 0.0f,
+        .y         = 0.0f,
+        .width     = 114.0f,
+        .height    = 200.0f,
+        .ship_type = SHIP1
+    };
+
+
+    Iter iter = {
+        .curr_registry = &scene->registry,
+        .curr_event = &pse,
+        .Ts = 0.0f,
+    };
+
+    Entity player = Ecs::broadcast_event<Entity, PlayerSpawnEvent>(&scene->registry, &iter);
 
     PlayerSystem::set_player_entity(player);
 
     srand(0);
-
+    
     for (int i = 0; i < 500; ++i)
-    {     
+    {
 
-        float x = 100000.0f * ( 2 * ((float)(rand()) / RAND_MAX - 0.5));
-        float y = 100000.0f * ( 2 * ((float)(rand()) / RAND_MAX - 0.5));
+        float x = 100000.0f * ( 2 * ((float)(rand()) / RAND_MAX - 0.5f));
+        float y = 100000.0f * ( 2 * ((float)(rand()) / RAND_MAX - 0.5f));
 
         float radius = 1000.0f * ( 2 * ((float)(rand()) / RAND_MAX - 0.5));
         float mass = 1E20 * ((float)(rand()) / RAND_MAX);
@@ -112,23 +106,31 @@ static void setup_scene(Application_data *app, scene *scene, const char *pwd)
 
         PlanetSpawnEvent planetSE = {x, y, 0.0f, 0.0f, radius, mass, PLANET1};
 
-        Ecs::broadcast_event<Entity>(scene->registry, &planetSE);
+
+        Iter iter2 = {
+            .curr_registry = &scene->registry,
+            .curr_event    = &planetSE,
+            .Ts            = 0.0f,
+        };
+
+        Ecs::broadcast_event<Entity, PlanetSpawnEvent>(&scene->registry, &iter2);
     }
+    
 }
 
 
-void init(Application_data *app, const char *pwd)
+void init(const char *pwd)
 {
-    scene *main_scene = Application::create_add_scene(app, "GamePlay_scene");
-    setup_scene(app, main_scene, pwd);
+    scene *main_scene = Application::create_add_scene("GamePlay_scene");
+    setup_scene(main_scene, pwd);
 
 
 
-    Application::run(app, main_scene, update, fixed_update, render);
+    Application::run(Application::Get(), main_scene); //TODO(Johan) deprecated
 }
 
 
-void clean(Application_data *app)
+void clean()
 {
 
 }
