f82e4480aa6461fae4da0ab496bf33132423aabb
Johan <jsbollll@gmail.com>
2022-07-13 19:37:13 +0200

    render now use groups

--- src/Game/systems/RenderSystem.cpp
+++ src/Game/systems/RenderSystem.cpp
@@ -6,17 +6,16 @@
 
 
 void RenderSystem::render(Iter *iter)
-{  
-    View<Transform> *transform_view = Ecs::get_view<Transform, SpriteComponent>(iter->curr_registry);
-    View<SpriteComponent> *sprite_view = Ecs::get_view<SpriteComponent, Transform>(iter->curr_registry);
+{
+    Group *group = Ecs::get_group<Transform, SpriteComponent>(iter->curr_registry);
 
-    for (Usize i = 0; i < transform_view->size; ++i)
+    for (Usize i = 0; i < group->size; ++i)
     {
         //dbg(printf("running in render for loop\n"));
 
 
-        Transform *transform = &transform_view->comparray[i];
-        SpriteComponent *sprite_comp = &sprite_view->comparray[i];
+        Transform *transform = &((Transform *)(group->comp_arrays[0]))[i];
+        SpriteComponent *sprite_comp = &((SpriteComponent *)(group->comp_arrays[1]))[i];
 
 
 
