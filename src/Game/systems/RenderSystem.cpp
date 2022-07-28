766c185b58340e2f9a3375f7f446148693dc7ff7
Johan-su <jsbollll@gmail.com>
2022-07-29 00:07:58 +0200

    change to better name

--- src/Game/systems/RenderSystem.cpp
+++ src/Game/systems/RenderSystem.cpp
@@ -2,10 +2,9 @@
 #include "../Components_Events.hpp"
 
 
-void RenderSystem::render(Iter *iter)
+void RenderSystem::render(Iter *it)
 {
-    Group *group = Ecs::get_group<Transform, SpriteComponent>(iter->registry);
-
+    Group *group = Ecs::get_group<Transform, SpriteComponent>(it->registry);
 
     Transform *transform_list    = Ecs::get_comp_array<Transform>(group, 0);
     SpriteComponent *sprite_list = Ecs::get_comp_array<SpriteComponent>(group, 1);
@@ -13,7 +12,7 @@ void RenderSystem::render(Iter *iter)
     for (Usize i = 0; i < group->size; ++i)
     {
 
-        Application::RenderCopyExF(iter->registry, &transform_list[i], &sprite_list[i]);
+        Application::RenderCopyExF(it->registry, &transform_list[i], &sprite_list[i]);
     }
 }
 
