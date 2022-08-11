e7ed1843088763400d49eaed1500d1cf36abe94a
Johan <jsbollll@gmail.com>
2022-08-11 19:58:45 +0200

    make use of firing system

--- src/Game/systems/AIControllerSystem.cpp
+++ src/Game/systems/AIControllerSystem.cpp
@@ -9,21 +9,26 @@
 void AIControllerSystem::update(Iter *it)
 {
 
-    Group *enemy_group = Ecs::get_group<Transform, EnemyAI>(it->registry);
+    const Group *enemy_group = Ecs::get_group<Transform, EnemyAI, FiringComponent>(it->registry);
 
-    Transform *t_list = Ecs::get_comp_array<Transform>(enemy_group, 0);
-    EnemyAI *Eai_list = Ecs::get_comp_array<EnemyAI>(enemy_group, 1);
+    const Transform *t_list = Ecs::get_comp_array<Transform>(enemy_group, 0);
+    const EnemyAI *Eai_list = Ecs::get_comp_array<EnemyAI>(enemy_group, 1);
     
 
 
 
-    Group *planet_group = Ecs::get_group<Transform, CircleCollider, Planet>(it->registry);
-
-    Transform *p_t_list = Ecs::get_comp_array<Transform>(planet_group, 0);
-    CircleCollider *p_cc_list = Ecs::get_comp_array<CircleCollider>(planet_group, 1);
+    const Group *planet_group = Ecs::get_group<Transform, CircleCollider, Planet>(it->registry);
 
 
-    Transform planet_transform = p_t_list[0];
+    Transform planet_transform = {};
+
+    if (planet_group->size == 1)
+    {
+        const Transform *p_t_list = Ecs::get_comp_array<Transform>(planet_group, 0);
+        const CircleCollider *p_cc_list = Ecs::get_comp_array<CircleCollider>(planet_group, 1);
+
+        planet_transform = p_t_list[0];
+    }
 
 
     for (Usize i = 0; i < enemy_group->size; ++i)
@@ -34,28 +39,29 @@ void AIControllerSystem::update(Iter *it)
             {
                 Entity e = enemy_group->entity_list[i];
                 Transform t = t_list[i];
+                Transform *t_mut = Ecs::get_component<Transform>(it->registry, e);
+                FiringComponent *fc_mut = Ecs::get_component<FiringComponent>(it->registry, e);
 
                 float dx = planet_transform.pos.x - t.pos.x;
                 float dy = planet_transform.pos.y - t.pos.y;
 
-                float angle = atan2(dy, dx);
+                float angle = atan2f(dy, dx);
 
+                t_mut->rot = {cosf(angle), sinf(angle)};
 
-                t_list->rot = {cosf(angle), sinf(angle)};
-
-                float speed = 100.0f;
-
-                BulletSpawnEvent bse = {
-                    .pos = t.pos,
-                    .rot = t.rot,
-                    .vel = t.rot * speed,
-                    .damage = 5.0f,
-                    .bullet_type = SHIP1,
-                };
-
-                Ecs::push_event<BulletSpawnEvent>(it->registry, &bse); //TODO(Johan): add firing component
+                fc_mut->firing = true;
 
             } break;
-        }   
+
+            case EnemyAiState::TargetAlly:
+            {
+
+            } break;
+
+            case EnemyAiState::TargetPlayer:
+            {
+
+            } break;
+        } 
     }
 }
