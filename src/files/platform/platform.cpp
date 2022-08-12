e44ad795b3df491988238f9b71e8b817eef50834
Johan <jsbollll@gmail.com>
2022-08-13 01:49:07 +0200

    add linux interface for platform

--- src/files/platform/platform.cpp
+++ src/files/platform/platform.cpp
@@ -74,3 +74,79 @@ void memory_map::free(void *address, Usize page_amount)
 }
 
 #endif
+
+
+
+
+
+
+
+
+
+#ifdef __linux__
+
+
+void Platform::init()
+{
+    Linux::init();
+}
+
+
+Usize Platform::get_page_size()
+{
+    return Linux::get_page_size();
+}
+
+
+Usize Platform::bytes_to_page_amount(Usize bytes)
+{
+    return 1 + (bytes / (get_page_size() + 1));
+}
+
+
+Usize Platform::page_amount_to_bytes(Usize page_amount)
+{
+    return page_amount * get_page_size();
+}
+
+
+U64 deltaTime::get_micro_time()
+{
+    return Linux::get_micro_time();
+}
+
+
+U64 deltaTime::get_mili_time()
+{
+    return Linux::get_mili_time();
+}
+
+
+U64 deltaTime::get_sec_time()
+{
+    return Linux::get_sec_time();
+}
+
+
+void *memory_map::reserve(void *address, Usize page_amount)
+{
+    return Linux::reserve(address, page_amount);
+}
+
+
+void *memory_map::commit(void *address, Usize page_amount)
+{
+    fprintf(stderr, "DEBUG: commit [%p, %llu]\n", address, page_amount);
+    return Linux::commit(address, page_amount);
+}
+
+
+void memory_map::free(void *address, Usize page_amount)
+{
+    Linux::free(address, page_amount);
+}
+
+
+
+
+#endif
