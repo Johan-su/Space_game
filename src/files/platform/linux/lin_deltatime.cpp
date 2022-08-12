16b23d70b50d78bfc7c395d6d285991bb4faa86f
Jonathan Ekberg <jodrim@gmail.com>
2022-08-13 01:42:19 +0200

    Improved for nanosecond precision

--- src/files/platform/linux/lin_deltatime.cpp
+++ src/files/platform/linux/lin_deltatime.cpp
@@ -1,32 +1,40 @@
-#include <bits/chrono.h>
-#include <ctime>
+#include <chrono>
 #ifdef __linux__
 
 #include "lin_deltatime.hpp"
-#include "cstdint"
+#include "chrono"
 #include "sys/time.h"
+
+using namespace std::chrono;
 
 uint64_t Linux::get_micro_time() 
 {
-    time_t now = time(nullptr);
-    time_t microseconds = now * 1_000_000;
+    using time_stamp = time_point<system_clock, microseconds>;
+
+    time_stamp ts = time_point_cast<microseconds>(system_clock::now());
+    auto microseconds = ts.time_since_epoch().count();
 
     return microseconds;
 }
 
 uint64_t Linux::get_mili_time() 
 {
-    time_t now = time(nullptr);
-    time_t milliseconds = now * 1_000;
+    using time_stamp = time_point<system_clock, milliseconds>;
+
+    time_stamp ts = time_point_cast<milliseconds>(system_clock::now());
+    auto milliseconds = ts.time_since_epoch().count();
 
     return milliseconds;
 }
 
 uint64_t Linux::get_sec_time() 
 {
-    time_t now = time(nullptr);
+    using time_stamp = time_point<system_clock, seconds>;
 
-    return now;
+    time_stamp ts = time_point_cast<seconds>(system_clock::now());
+    auto seconds = ts.time_since_epoch().count();
+
+    return seconds;
 }
 
 #endif
