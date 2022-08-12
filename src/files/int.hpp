e4652409b1a81a0857ee3b1e2b82e2deb5288bd5
Johan <jsbollll@gmail.com>
2022-08-13 00:37:56 +0200

    fix int types for linux

--- src/files/int.hpp
+++ src/files/int.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+
+#ifdef _WIN64
 typedef signed char I8;
 typedef short       I16;
 typedef int         I32;
@@ -14,3 +16,26 @@ typedef unsigned int       U32;
 typedef unsigned long long U64;
 
 typedef unsigned long long Usize;
+
+#endif
+
+#ifdef __linux__
+#include <stdio.h>
+
+typedef int8_t  I8;
+typedef int16_t I16;
+typedef int32_t I32;
+typedef int64_t I64;
+
+typedef int64_t Isize;
+
+
+typedef uint8_t  U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
+
+typedef size_t   Usize;
+
+
+#endif
