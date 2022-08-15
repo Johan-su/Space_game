12fe3cbeae4c1f44b4f29c8203c828c49fa5ab49
Johan <jsbollll@gmail.com>
2022-08-15 17:06:53 +0200

    added vector3f, vector4f

--- src/files/math/math_types.cpp
+++ src/files/math/math_types.cpp
@@ -11,7 +11,6 @@ float Vector2f::magnitude()
 }
 
 
-
 void Vector2f::normalize()
 {
     float mag = this->magnitude();
@@ -25,8 +24,93 @@ void Vector2f::normalize()
 }
 
 
-
 float Vector2f::dot(Vector2f other_v2f)
 {
     return this->x * other_v2f.x + this->y * other_v2f.y;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+float Vector3f::magnitude()
+{
+    return sqrtf(this->x * this->x + this->y * this->y + this->z * this->z);
+}
+
+
+void Vector3f::normalize()
+{
+    float mag = this->magnitude();
+
+    if (mag < 0.0001f) // must be a zero vector
+        return;
+
+    this->x = this->x / mag;
+    this->y = this->y / mag;
+    this->z = this->z / mag;
+
+}
+
+
+float Vector3f::dot(Vector3f other_v3f)
+{
+    return this->x * other_v3f.x + this->y * other_v3f.y + this->z * other_v3f.z;
+}
+
+
+
+
+
+
+
+
+
+
+float Vector4f::magnitude()
+{
+    return sqrtf(this->x * this->x + this->y * this->y + this->z * this->z + this->w * this->w);
+}
+
+
+void Vector4f::normalize()
+{
+    float mag = this->magnitude();
+
+    if (mag < 0.0001f) // must be a zero vector
+        return;
+
+    this->x = this->x / mag;
+    this->y = this->y / mag;
+    this->z = this->z / mag;
+    this->w = this->w / mag;
+
+}
+
+
+float Vector4f::dot(Vector4f other_v4f)
+{
+    return this->x * other_v4f.x + this->y * other_v4f.y + this->z * other_v4f.z + this->w * other_v4f.w;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/files/math/math_types.hpp
+++ src/files/math/math_types.hpp
@@ -27,3 +27,60 @@ public:
     void normalize();
     float dot(Vector2f v2f);
 };
+
+
+class Vector3f
+{
+public:
+    float x;
+    float y;
+    float z;
+
+    Vector3f operator+(Vector3f v3f) { return Vector3f {this->x + v3f.x, this->y + v3f.y, this->z + v3f.z}; }
+    Vector3f operator-(Vector3f v3f) { return Vector3f {this->x - v3f.x, this->y - v3f.y, this->z - v3f.z}; }
+
+
+    Vector3f operator+(float scalar)  { return Vector3f {this->x + scalar, this->y + scalar, this->z + scalar}; }
+    Vector3f operator-(float scalar)  { return Vector3f {this->x - scalar, this->y - scalar, this->z - scalar}; }
+    Vector3f operator*(float scalar)  { return Vector3f {this->x * scalar, this->y * scalar, this->z * scalar}; }
+    Vector3f operator/(float scalar)  { return Vector3f {this->x / scalar, this->y / scalar, this->z / scalar}; }
+
+    void operator+=(float scalar) { *this = *this + scalar; }
+    void operator-=(float scalar) { *this = *this - scalar; }
+    void operator*=(float scalar) { *this = *this * scalar; }
+    void operator/=(float scalar) { *this = *this / scalar; }
+
+
+    float magnitude();
+    void normalize();
+    float dot(Vector3f v3f);
+};
+
+
+class Vector4f
+{
+public:
+    float x;
+    float y;
+    float z;
+    float w;
+
+    Vector4f operator+(Vector4f v4f) { return Vector4f {this->x + v4f.x, this->y + v4f.y, this->z + v4f.z, this->w + v4f.w}; }
+    Vector4f operator-(Vector4f v4f) { return Vector4f {this->x - v4f.x, this->y - v4f.y, this->z - v4f.z, this->w - v4f.w}; }
+
+
+    Vector4f operator+(float scalar)  { return Vector4f {this->x + scalar, this->y + scalar, this->z + scalar, this->w + scalar}; }
+    Vector4f operator-(float scalar)  { return Vector4f {this->x - scalar, this->y - scalar, this->z - scalar, this->w - scalar}; }
+    Vector4f operator*(float scalar)  { return Vector4f {this->x * scalar, this->y * scalar, this->z * scalar, this->w * scalar}; }
+    Vector4f operator/(float scalar)  { return Vector4f {this->x / scalar, this->y / scalar, this->z / scalar, this->w / scalar}; }
+
+    void operator+=(float scalar) { *this = *this + scalar; }
+    void operator-=(float scalar) { *this = *this - scalar; }
+    void operator*=(float scalar) { *this = *this * scalar; }
+    void operator/=(float scalar) { *this = *this / scalar; }
+
+
+    float magnitude();
+    void normalize();
+    float dot(Vector4f v4f);
+};
