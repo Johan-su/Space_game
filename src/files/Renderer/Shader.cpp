071f0b1d10767bb48ce17acd746c56b04b652647
Johan <jsbollll@gmail.com>
2022-08-18 20:45:18 +0200

    parse and map all uniforms in shaders

--- src/files/Renderer/Shader.cpp
+++ src/files/Renderer/Shader.cpp
@@ -54,8 +54,88 @@ static U32 compile_shader(U32 type, const char *src)
 }
 
 
+static bool is_whitespace(const char val)
+{
+    switch (val)
+    {
+        case '\r': return true;
+        case '\n': return true;
+        case '\t': return true;
+        case '\b': return true;
+        case ' ':  return true;
+
+        default: return false;        
+    }
+}
+
+
+static void move_past_whitespace(const char *src, U32 *index)
+{
+    while (is_whitespace(*(src + *index)))
+    {
+        *index += 1;
+    }
+}
+
+
+static bool is_word(const char *src, const char *word)
+{
+
+    while (*word != '\0')
+    {
+        if (*src != *word)
+        {
+            return false;
+        }
+        src += 1;
+        word += 1;
+    }
+    return true;
+}
+
+
+static void parse_and_add_uniforms(Shader *shader, const char *src, ShaderType shader_type)
+{
+    for (U32 index = 0;*(src + index) != '\0'; ++index)
+    {
+        if (is_word(src + index, "uniform"))
+        {
+            UniformElement element = {
+                .shader_type = shader_type,
+            };
+
+            index += strlen("uniform");
+
+            move_past_whitespace(src, &index);
+
+            for (U32 i = 0; !is_whitespace(*(src + index)); ++i)
+            {
+                element.type[i] = *(src + index);
+                index += 1;
+            }
+
+            move_past_whitespace(src, &index);
+
+            for (U32 i = 0; !is_whitespace(*(src + index)) && !is_word(src + index, "[") && !is_word(src + index, ";"); ++i)
+            {
+                element.name[i] = *(src + index);
+                index += 1;
+            }
+
+            element.location =  glGetUniformLocation(shader->id, element.name);
+            
+            HashMapN::set(&shader->uniform_map, HashMapN::hash_string(element.name), element);
+        }
+    }
+}
+
+
+
 void Real::init_shader(Shader *shader, const char *vert_src, const char *frag_src)
 {
+    HashMapN::init(&shader->uniform_map);
+
+
     shader->id = glCreateProgram();
 
     U32 vs = compile_shader(GL_VERTEX_SHADER, vert_src);
@@ -75,6 +155,10 @@ void Real::init_shader(Shader *shader, const char *vert_src, const char *frag_sr
 
     glDeleteShader(vs);
     glDeleteShader(fs);
+
+
+    parse_and_add_uniforms(shader, vert_src, ShaderType::Vertex);
+    parse_and_add_uniforms(shader, frag_src, ShaderType::Fragment);
 }
 
 
@@ -89,4 +173,66 @@ void Real::bind(Shader *shader)
 void Real::unbind(Shader *shader)
 {
     glUseProgram(0);
+}
+
+
+
+
+void Real::set_uniform_I32(Shader *shader, I32 value, const char *name)
+{
+    U64 hash = HashMapN::hash_string(name);
+    UniformElement *element = HashMapN::get_pointer(&shader->uniform_map, hash);
+    if (element == nullptr)
+    {
+        fprintf(stderr, "WARNING: could not find uniform %s", name);
+        return;
+    }
+    
+    bind(shader);
+    glUniform1i(element->location, value);
+}
+
+
+void Real::set_uniform_float(Shader *shader, float value, const char *name)
+{
+    U64 hash = HashMapN::hash_string(name);
+    UniformElement *element = HashMapN::get_pointer(&shader->uniform_map, hash);
+    if (element == nullptr)
+    {
+        fprintf(stderr, "WARNING: could not find uniform %s", name);
+        return;
+    }
+
+    bind(shader);
+    glUniform1f(element->location, value);
+}
+
+
+void Real::set_uniform_vec3f(Shader *shader, Vector3f value, const char *name)
+{
+    U64 hash = HashMapN::hash_string(name);
+    UniformElement *element = HashMapN::get_pointer(&shader->uniform_map, hash);
+    if (element == nullptr)
+    {
+        fprintf(stderr, "WARNING: could not find uniform %s", name);
+        return;
+    }
+
+    bind(shader);
+    glUniform3f(element->location, value.x, value.y, value.z);
+}
+
+
+void Real::set_uniform_vec4f(Shader *shader, Vector4f value, const char *name)
+{
+    U64 hash = HashMapN::hash_string(name);
+    UniformElement *element = HashMapN::get_pointer(&shader->uniform_map, hash);
+    if (element == nullptr)
+    {
+        fprintf(stderr, "WARNING: could not find uniform %s", name);
+        return;
+    }
+
+    bind(shader);
+    glUniform4f(element->location, value.x, value.y, value.z, value.w);
 }
--- src/files/Renderer/Shader.hpp
+++ src/files/Renderer/Shader.hpp
@@ -1,10 +1,35 @@
 #pragma once
 
 #include "../int.hpp"
+#include "../datastructures/hashmap.hpp"
+
+
+#include "../math/math_types.hpp"
+
+
+#define MAX_UNIFORM_TYPE_LENGTH 64
+#define MAX_UNIFORM_NAME_LENGTH 128
+
+enum class ShaderType
+{
+    Vertex,
+    Fragment,
+};
+
+
+struct UniformElement
+{
+    ShaderType shader_type;
+    I32 location;
+    char type[MAX_UNIFORM_TYPE_LENGTH];
+    char name[MAX_UNIFORM_NAME_LENGTH];
+};
+
 
 struct Shader
 {
     U32 id;
+    HashMap<UniformElement> uniform_map;
 };
 
 
@@ -15,6 +40,13 @@ namespace Real
 
    void bind(Shader *shader);
    void unbind(Shader *shader);
+
+
+    void set_uniform_I32(Shader *shader, I32 value, const char *name);
+    void set_uniform_float(Shader *shader, float value, const char *name);
+    void set_uniform_vec3f(Shader *shader, Vector3f value, const char *name);
+    void set_uniform_vec4f(Shader *shader, Vector4f value, const char *name);
+
 } // namespace Real
 
 
