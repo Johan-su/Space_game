750b9eb2925bfe6036f37d3f87bb1051e6849de5
Johan <jsbollll@gmail.com>
2023-01-27 00:19:44 +0100

    refactor eventmanager

--- src/files/ecs/Registry.cpp
+++ src/files/ecs/Registry.cpp
@@ -3,6 +3,44 @@
 using namespace Ecs;
 
 
+
+
+void Event_functions::init(event_data *ed)
+{
+    memset(ed, 0, sizeof(*ed));
+}
+
+
+void Event_functions::run_events(event_data *ed, Memory_arena *event_mm, Iter *it)
+{
+    void *after_latest_event = event_mm->data;
+
+    while (ed->events_in_buffer_count != 0)
+    {
+        Usize *event_id = (Usize *)after_latest_event;
+        void *event = event_id + 1;
+        ECS_assert(ed->event_init[*event_id], "event not initalized");
+        void *after_latest_event_unaligned = (U8 *)event + ed->type_data[*event_id].size;
+
+        after_latest_event = Utils::align_pointer(after_latest_event_unaligned, alignof(Usize));
+
+        for (int i = 0; i < ed->listener_data[*event_id].event_sub_count; ++i)
+        {
+            it->event = event;
+            ed->listener_data[*event_id].event_listeners[i](it);
+        }
+        --ed->events_in_buffer_count;
+    }
+    Arena::clear_arena(event_mm);
+}
+
+
+
+
+
+
+
+
 void Ecs::init(Registry *registry, Memory_arena *mm, Memory_arena *view_mm, Memory_arena *event_mm)
 {
     ECS_assert(registry != nullptr, "Registry cannot be nullptr");
