388f3a1f367708ea4058e004f4a7396fd7da3788
Johan <jsbollll@gmail.com>
2023-01-27 16:34:48 +0100

    refactor vertexBuffer

--- src/files/Renderer/VertexArray.cpp
+++ src/files/Renderer/VertexArray.cpp
@@ -1,7 +1,41 @@
 #include "VertexArray.hpp"
 
 
+
+
+
 #include "../core/loadgl/load_gl.hpp"
+
+
+
+void Real::init_vbuffer(VertexBuffer *vb, Usize buffer_size, const void *data)
+{
+    glGenBuffers(1, &vb->id);
+    glBindBuffer(GL_ARRAY_BUFFER, vb->id);
+    glBufferData(GL_ARRAY_BUFFER, buffer_size, data, GL_STATIC_DRAW);
+}
+
+
+void Real::clean_vbuffer(VertexBuffer *vb)
+{
+    glDeleteBuffers(1, &vb->id);
+}
+
+
+void Real::bind(VertexBuffer *vb)
+{
+    glBindBuffer(GL_ARRAY_BUFFER, vb->id);
+}
+
+
+void Real::unbind(VertexBuffer *vb)
+{
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+
+
+
 
 void Real::init_vArray(VertexArray *va)
 {
--- src/files/Renderer/VertexArray.hpp
+++ src/files/Renderer/VertexArray.hpp
@@ -2,7 +2,24 @@
 
 #include "../int.hpp"
 
-#include "VertexBuffer.hpp"
+
+struct VertexBuffer
+{
+    U32 id;
+};
+
+
+
+namespace Real
+{
+    void init_vbuffer(VertexBuffer *vb, Usize buffer_size, const void *data);
+    void clean_vbuffer(VertexBuffer *vb);
+    void bind(VertexBuffer *vb);
+    void unbind(VertexBuffer *vb);    
+} // namespace Real
+
+
+
 #include "VertexLayout.hpp"
 
 struct VertexArray
